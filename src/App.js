3ca5f5e62c49a6fa779d65f3a90622d87bf78a14
Jasurbek Aripov <68148826+aripovjr@users.noreply.github.com>
2021-12-27 22:46:12 +0300

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Setup from './tours'
+import './index.css'
+
+
+function App(){
+    return(
+    <>
+    <div className='container'>
+            <Setup />
+    </div>
+    </>
+)
+}
+
+export default App
